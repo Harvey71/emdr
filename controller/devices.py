48fe32d17fa47e16151d8d58f9c934fcc7fd19bb
Thomas Niebur <mail@thomas-niebur.de>
2020-01-21 01:05:06 +0100

    fix for windows (is_kernel_driver_active is not implemented)

--- controller/devices.py
+++ controller/devices.py
@@ -1,6 +1,7 @@
 import usb
 import pygame
 from array import array
+import os
 
 
 class Note(pygame.mixer.Sound):
@@ -46,8 +47,9 @@ class Devices():
         for dev in devs:
             try:
                 #print(repr(dev))
-                if dev.is_kernel_driver_active(0):
-                    dev.detach_kernel_driver(0)
+                if os.name != 'nt':
+                    if dev.is_kernel_driver_active(0):
+                        dev.detach_kernel_driver(0)
                 dev.write(0x03, 'i\n')
                 id_arr = dev.read(0x84, size_or_buffer=64, timeout=100)
                 id_str = ''.join(chr(x) for x in id_arr).strip()
