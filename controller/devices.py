5084e02e94d1a66021b14fccefc6fa98ea134722
Thomas Niebur <mail@thomas-niebur.de>
2023-04-20 17:14:47 +0200

    Python 3.9 compatibility

--- controller/devices.py
+++ controller/devices.py
@@ -47,10 +47,11 @@ class Devices():
         for dev in devs:
             try:
                 # print(repr(dev))
-                if platform.system() == 'Linux':
-                    if dev.is_kernel_driver_active(0):
-                        dev.detach_kernel_driver(0)
-                dev.write(0x03, 'i\n')
+                try:
+                    dev.write(0x03, 'i\n')
+                except usb.core.USBError:
+                    dev.detach_kernel_driver(0)
+                    dev.write(0x03, 'i\n')
                 id_arr = dev.read(0x84, size_or_buffer=64, timeout=100)
                 id_str = ''.join(chr(x) for x in id_arr).strip()
                 #print(id_str)
