9f8644c032d2f1e63a6d6161b69bb60af056d5e6
Tres Popp <tpopp@brium.ai>
2025-02-22 17:11:43 +0100

    This adds the config that I saw with a D1 Mini with USB C

--- controller/device_config.py
+++ controller/device_config.py
@@ -3,5 +3,6 @@ DEVICE_CONFIG = {
 	'Teensy 4.0':        {'vid': 61525, 'pid': 38914, 'baud': 115200, 'echo': False},
 	'Raspberry Pi Pico': {'vid': 11914, 'pid':     5, 'baud': 115200, 'echo': True},
 	'ESP D1 Mini':       {'vid':  6790, 'pid': 29987, 'baud': 115200, 'echo': True},
+	'ESP-12F D1 Mini':   {'vid': 1027,  'pid': 24577, 'baud': 115200, 'echo': True},
 }
 
